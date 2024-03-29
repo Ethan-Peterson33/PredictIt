9e7c5bf0c08b9f2e1b00cf274fb4f65fa2ba0766
Ethan Peterson <48261681+Ethan-Peterson33@users.noreply.github.com>
2024-03-29 15:47:48 -0400

    Update variables.py

--- app_files/variables.py
+++ app_files/variables.py
@@ -38,21 +38,63 @@ markets_schema = [
 
 query = """
 
-insert into predict-it-dezoomcamp.PredictIt.markets_contracts
+CREATE TABLE IF NOT EXISTS `predict-it-dezoomcamp.PredictIt.markets_contracts` (
+    market_id INTEGER,
+    market STRING,
+    market_name STRING,
+    market_image STRING,
+    url STRING,
+    currentdate DATE,
+    timestamp DATETIME,
+    market_status STRING,
+    contract_id INTEGER,
+    contract_image STRING,
+    contract_name STRING,
+    contract_short STRING,
+    contract_status STRING,
+    lasttradeprice FLOAT64,
+    bestbuyyescost FLOAT64,
+    bestbuynocost FLOAT64,
+    bestsellyescost FLOAT64,
+    bestsellnocost FLOAT64,
+    lastcloseprice FLOAT64
+);
+
+INSERT INTO `predict-it-dezoomcamp.PredictIt.markets_contracts` (
+    market_id,
+    market,
+    market_name,
+    market_image,
+    url,
+    currentdate,
+    timestamp,
+    market_status,
+    contract_id,
+    contract_image,
+    contract_name,
+    contract_short,
+    contract_status,
+    lasttradeprice,
+    bestbuyyescost,
+    bestbuynocost,
+    bestsellyescost,
+    bestsellnocost,
+    lastcloseprice
+)
 SELECT
     c.market_id,
-    m.name as market,
-    m.name as market_name,
-    m.image as market_image,
+    m.name AS market,
+    m.name AS market_name,
+    m.image AS market_image,
     m.url,
     CURRENT_DATE() AS currentdate,
-    cast(PARSE_TIMESTAMP("%Y-%m-%dT%H:%M:%E*S",m.timestamp) AS DATETIME) AS timestamp,  -- Convert TIMESTAMP to DATETIME
-    m.status as market_status,
-    c.id as contract_id,
-    c.image as contract_image,
-    c.name as contract_name,
-    c.shortname as contract_short,
-    c.status as contract_status,
+    CAST(PARSE_TIMESTAMP("%Y-%m-%dT%H:%M:%E*S", m.timestamp) AS DATETIME) AS timestamp,
+    m.status AS market_status,
+    c.id AS contract_id,
+    c.image AS contract_image,
+    c.name AS contract_name,
+    c.shortname AS contract_short,
+    c.status AS contract_status,
     c.lasttradeprice,
     c.bestbuyyescost,
     c.bestbuynocost,
@@ -62,7 +104,8 @@ SELECT
 FROM 
     `predict-it-dezoomcamp.PredictIt.markets` m 
 INNER JOIN 
-    `predict-it-dezoomcamp.PredictIt.contracts` c ON c.market_id = m.id
+    `predict-it-dezoomcamp.PredictIt.contracts` c ON c.market_id = m.id;
+
         
 
 """
